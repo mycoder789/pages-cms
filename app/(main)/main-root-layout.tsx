98254622cee38a6ed0e46547c1dd2b8e3bc72c18
mycoder789 <alitatechuology@163.com>
2025-05-20 22:39:48 +0800

    更新 main-root-layout.tsx

--- app/(main)/main-root-layout.tsx
+++ app/(main)/main-root-layout.tsx
@@ -1,16 +1,20 @@
 import { User } from "@/components/user";
 import { About } from "@/components/about";
 
+// 页面主布局：包括顶部内容区域与底部信息栏
 export function MainRootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode; // 子组件内容
 }) {
 	return(
     <div className="flex flex-col h-screen">
 			<main className="flex-1 w-full overflow-auto">
+				{/* 主体区域，用于渲染页面内容 */}
 				{children}
 			</main>
+
+			{/* 页面底部，包含用户信息和“关于”组件 */}
 			<footer className="flex items-center gap-2 border-t px-2 py-2 lg:px-4 lg:py-3 mt-auto">
         <User className="mr-auto"/>
         <About/>
