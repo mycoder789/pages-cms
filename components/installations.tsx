81e9715c3d2448c9f84c688b9e423c1acd6a7024
mycoder789 <alitatechuology@163.com>
2025-05-20 23:00:53 +0800

    更新 installations.tsx

--- components/installations.tsx
+++ components/installations.tsx
@@ -11,7 +11,7 @@ const Installations = () => {
     return (
       <div className="text-sm text-muted-foreground h-[50px] px-6 flex justify-center items-center bg-accent rounded-md">
         <Ban className="h-4 w-4 mr-2"/>
-        No account with the Github application installed.
+        未检测到已安装 GitHub 应用的账号。
       </div>
     );
   }
@@ -21,7 +21,7 @@ const Installations = () => {
       {user.accounts.map(account =>
         <li className="flex items-center gap-x-3 border border-b-0 last:border-b first:rounded-t-md last:rounded-b-md px-3 py-2 text-sm" key={account.login}>
           <div className="flex gap-x-2 items-center">
-            <img src={`https://github.com/${account.login}.png`} alt={`${account.login}'s avatar`} className="h-6 w-6 rounded"/>
+            <img src={`https://github.com/${account.login}.png`} alt={`${account.login} 的头像`} className="h-6 w-6 rounded"/>
             <span className="font-medium truncate">{account.login}</span>
           </div>
           <Button size="sm" variant="outline" className="h-8 ml-auto" asChild>
@@ -31,7 +31,7 @@ const Installations = () => {
                 : `https://github.com/settings/installations/${account.installationId ?? ''}`} 
               target="_blank"
             >
-              Manage
+              管理
               <ArrowUpRight className="h-3 w-3 ml-1 opacity-50" />
             </a>
           </Button>
