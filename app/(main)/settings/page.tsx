917e8bf204555f1b6c3e4b7852eb4c5573710bcf
mycoder789 <alitatechuology@163.com>
2025-05-20 22:41:51 +0800

    更新 page.tsx

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -34,23 +34,23 @@ export default async function Page() {
       <div className="max-w-screen-sm mx-auto p-4 md:p-6 space-y-6">
         <Link className={cn(buttonVariants({ variant: "outline", size: "xs" }), "inline-flex")} href="/" prefetch={true}>
           <ArrowLeft className="h-4 w-4 mr-1.5" />
-          Go home
+          返回首页
         </Link>
         <header className="flex items-center mb-6">
-          <h1 className="font-semibold tracking-tight text-lg md:text-2xl">Settings</h1>
+          <h1 className="font-semibold tracking-tight text-lg md:text-2xl">设置</h1>
         </header>
         <div className="flex flex-col relative flex-1 space-y-6">
           <Card>
             <CardHeader>
-              <CardTitle className="text-base md:text-lg">Profile</CardTitle>
-              <CardDescription>Manage the information displayed to other users.</CardDescription>
+              <CardTitle className="text-base md:text-lg">个人资料</CardTitle>
+              <CardDescription>管理展示给其他用户的信息。</CardDescription>
             </CardHeader>
             <CardContent>
               <form className="w-full">
                 <div className="grid w-full items-center gap-4">
                   <div className="grid grid-cols-4 items-center gap-4">
                     <Label htmlFor="name" className="text-right">
-                      Name
+                      姓名
                     </Label>
                     <div className="col-span-3">
                       <Input name="name" disabled defaultValue={displayName}/>
@@ -58,7 +58,7 @@ export default async function Page() {
                   </div>
                   <div className="grid grid-cols-4 items-center gap-4">
                     <Label htmlFor="picture" className="text-right">
-                      Picture
+                      头像
                     </Label>
                     <div className="col-span-3">
                       <Avatar className="h-24 w-24 rounded-md">
@@ -82,15 +82,15 @@ export default async function Page() {
               </form>
             </CardContent>
             <CardFooter>
-              <Button size="sm" className="ml-auto" disabled>Save profile</Button>
+              <Button size="sm" className="ml-auto" disabled>保存资料</Button>
             </CardFooter>
           </Card>
           
           {user.githubId &&
             <Card>
               <CardHeader>
-                <CardTitle className="text-base md:text-lg">Installations</CardTitle>
-                <CardDescription>Manage the accounts the Github application is installed on.</CardDescription>
+                <CardTitle className="text-base md:text-lg">GitHub 安装</CardTitle>
+                <CardDescription>管理你安装了 GitHub 应用的账户。</CardDescription>
               </CardHeader>
               <CardContent>
                 <Installations/>
