a4399d830a72f637d87d09fce0e10271191d266e
mycoder789 <alitatechuology@163.com>
2025-05-20 22:44:24 +0800

    更新 about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -30,30 +30,33 @@ export function About({
           <DialogTrigger asChild>
             <Button variant="ghost" size="icon">
               <CircleHelp className="h-4 w-4" />
-              <span className="sr-only">About Pages CMS</span>
+              <span className="sr-only">关于 Pages CMS</span>
             </Button>
           </DialogTrigger>
         </TooltipTrigger>
         <TooltipContent>
-          About Pages CMS</TooltipContent>
+          关于 Pages CMS
+        </TooltipContent>
       </Tooltip>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>About Pages CMS</DialogTitle>
-          <DialogDescription>Pages CMS is an Open Source Content Management System built for static websites (Jekyll, Next.js, VuePress, Hugo, etc). It allows you to edit your website&apos;s content directly on GitHub via a user-friendly interface.</DialogDescription>
+          <DialogTitle>关于 Pages CMS</DialogTitle>
+          <DialogDescription>
+            Pages CMS 是一个为静态网站（如 Jekyll、Next.js、VuePress、Hugo 等）设计的开源内容管理系统。它允许你通过用户友好的界面，直接在 GitHub 上编辑你的网站内容。
+          </DialogDescription>
         </DialogHeader>
         <footer className="grid grid-flow-col justify-stretch text-sm gap-x-2">
           <a className={cn(buttonVariants({ variant: "outline", size: "sm" }), "w-full")} href="https://pagescms.org" target="_blank">
             <Chrome className="h-4 w-4 shrink-0 mr-2"/>
-            Website
+            官方网站
           </a>
           <a className={cn(buttonVariants({ variant: "outline", size: "sm" }), "w-full")} href="https://pagescms.org/docs" target="_blank">
             <Book className="h-4 w-4 shrink-0 mr-2"/>
-            Docs
+            使用文档
           </a>
           <a className={cn(buttonVariants({ variant: "outline", size: "sm" }), "w-full")} href="https://github.com/pages-cms/pages-cms" target="_blank">
             <Github className="h-4 w-4 shrink-0 mr-2"/>
-            GitHub
+            GitHub 项目
           </a>
         </footer>
       </DialogContent>
